8ad63d0614fcf218d051d1103a4e753e1012de05
lethangomes <35245543+lethangomes@users.noreply.github.com>
2022-07-16 17:01:41 -0700

    Adjusted Aim Assist, made it less aggressive

--- Assets/Scripts/AimAssistCone.cs
+++ Assets/Scripts/AimAssistCone.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class AimAssistCone : MonoBehaviour
 {
+    Transform projectile;
     // Start is called before the first frame update
     void Start()
     {
-        
+        projectile = transform.root;
     }
 
     // Update is called once per frame
@@ -18,22 +19,38 @@ public class AimAssistCone : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
-        if(col.tag == "target")
+        if(col.tag == "Cut" || col.tag == "Cuttable" || col.tag == "Explosive")
         {
-            //unattaches blade from projectile so blade doesn't rotate
-            Transform child = transform.root.Find("Blade Beam(Clone)");
-            child.parent = null;
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+            {//was going to hit target anyways
+                
+            }
+            else
+            {
+                //unattaches blade from projectile so blade doesn't rotate
+                Transform child = transform.root.Find("Blade Beam(Clone)");
+                child.parent = null;
 
-            //points projectile at object within cone
-            transform.root.LookAt(col.gameObject.transform);
-            transform.root.Rotate(0, 180, 0);
+                //stores inital blade angle
+                Quaternion initialAngle = projectile.rotation;
 
-            //resets projetile trajectory
-            transform.root.gameObject.GetComponent<bladeBeam>().timeAltered(Time.timeScale);
+                //points projectile at object within cone
+                projectile.LookAt(col.gameObject.transform);
+                projectile.Rotate(0, 180, 0);
+                Quaternion newRotation = Quaternion.Slerp(initialAngle, projectile.rotation, 0.75f);
+                //newRotation.eulerAngles = new Vector3((initialAngle.x + projectile.rotation.eulerAngles.x) / 2, (initialAngle.y + projectile.rotation.eulerAngles.y) / 2, (initialAngle.z + projectile.rotation.eulerAngles.z) / 2);
+                projectile.rotation = newRotation;
 
-            //reattaches blade to projectile
-            child.SetParent(transform.root);
+                //resets projetile trajectory
+                projectile.gameObject.GetComponent<bladeBeam>().timeAltered(Time.timeScale);
+
+                //reattaches blade to projectile
+                child.SetParent(projectile);
+                
+            }
             Destroy(gameObject);
+
         }
     }
 }
